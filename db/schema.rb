f99519717c108e2b3a698c2c7f3b3f5ad6d0d3fb
Alícia Fiúza <56484179+aliciafiuzaa@users.noreply.github.com>
2021-07-04 01:20:02 +0100

    Add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_182853) do
+ActiveRecord::Schema.define(version: 2021_07_03_232546) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "user_comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
